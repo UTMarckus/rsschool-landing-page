3caf0abfb5c1a8d1a868a01ee7137876d5240208
UTMarckus <utmarckus@gmail.com>
2026-09-26 09:56:58 +0300

    feat: add burger behavior

---
+++ js/burger.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const menuNav = document.querySelector('.nav');
+const burgerButton = document.querySelector('.burger-menu');
+
+burgerButton.addEventListener('click', onBurgerClick);
+
+menuNav.addEventListener('click', onMenuClick);
+
+function onBurgerClick(event) {
+  burgerButton.classList.toggle('_open');
+  menuNav.classList.toggle('_open');
+  document.body.classList.toggle('_lock');
+}
+
+function onMenuClick(event) {
+  burgerButton.classList.remove('_open');
+  menuNav.classList.remove('_open');
+  document.body.classList.remove('_lock')
+}
